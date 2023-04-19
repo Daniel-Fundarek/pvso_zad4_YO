aac226fefb55b92859c70d0b0d7c4f4d01b2e6e5
Daniel-Fundarek <xfundarekd@stuba.sk>
2023-04-19 14:57:33 +0200

    DBSCAN clustering

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # This is a sample Python script.
 import numpy as np
 import open3d as o3d
-from sklearn.cluster import KMeans, DBSCAN, OPTICS
+from sklearn.cluster import KMeans, DBSCAN, OPTICS, MeanShift
 import matplotlib.pyplot as plt
+from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.preprocessing import StandardScaler
 import io
 
@@ -20,8 +21,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-    # pcd = o3d.io.read_point_cloud("pvso/pointcloud/internet_pcd.ply",format='ply')
-    pcd = o3d.io.read_point_cloud("pvso/pointcloud/output_big4.ply", format='ply')
+    pcd = o3d.io.read_point_cloud("pvso/pointcloud/internet_pcd.ply",format='ply')
+    # pcd = o3d.io.read_point_cloud("pvso/pointcloud/output_big6.ply", format='ply')
 
     # Convert PCD point cloud to PLY format in memory
     # ply_str = io.StringIO()
@@ -33,7 +34,7 @@ if __name__ == '__main__':
 
     print(pcd)
     # print(np.asarray(pcd.points))
-    o3d.visualization.draw_geometries([pcd])
+    # o3d.visualization.draw_geometries([pcd])
 
     cl, inliers = pcd.remove_statistical_outlier(nb_neighbors=20,
                                                  std_ratio=2.0)
@@ -42,13 +43,19 @@ if __name__ == '__main__':
     outlier_cloud = pcd.select_by_index(inliers, invert=True)
     # inlier_cloud.paint_uniform_color([1, 0, 0])
     outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
-    o3d.visualization.draw_geometries([inlier_cloud])
+    # o3d.visualization.draw_geometries([inlier_cloud])
 
-    points = np.asarray(inlier_cloud.points).copy()
+    pcd = inlier_cloud
+    points = np.asarray(pcd.points)
+
+    # Normalisation:
     scaled_points = StandardScaler().fit_transform(points)
+
     # Clustering:
-    model = KMeans(n_clusters=10)
+    model = DBSCAN(eps=0.15, min_samples=10)
+    # model = KMeans(n_clusters=4)
     model.fit(scaled_points)
+
     # Get labels:
     labels = model.labels_
     # Get the number of colors:
@@ -59,45 +66,7 @@ if __name__ == '__main__':
     # Attribute to noise the black color:
     colors[labels < 0] = 0
     # Update points colors:
-    inlier_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
-    o3d.visualization.draw_geometries([inlier_cloud])
+    pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
 
-    # o3d.io.write_point_cloud("pvso/pointcloud/output_big1_inliers.ply", inlier_cloud, write_ascii=True)
-    # pcd = o3d.io.read_point_cloud("pvso/pointcloud/output_big1_inliers.ply")
-    # pcd = inlier_cloud
-    # points = np.asarray(pcd.points)
-    # # Define k-means parameters
-    # max_clusters = 10
-    # max_iters = 100
-    #
-    # # Perform k-means with a range of k values
-    # inertias = []
-    # for k in range(1, max_clusters + 1):
-    #     kmeans = KMeans(n_clusters=k, max_iter=max_iters).fit(points)
-    #     inertias.append(kmeans.inertia_)
-    #
-    # # Use elbow method to estimate optimal number of clusters
-    # diff = np.diff(inertias)
-    # plt.plot(range(1, max_clusters + 1), inertias, '-o')
-    # plt.plot(range(1, max_clusters), diff, '-o')
-    # plt.xlabel('Number of clusters (k)')
-    # plt.ylabel('Inertia')
-    # plt.title('Inertia vs. number of clusters')
-    # plt.xticks(range(1, max_clusters + 1))
-    # plt.show()
-    #
-    # # Choose optimal number of clusters and perform k-means again
-    # optimal_k = np.argmin(diff) + 1
-    # kmeans = KMeans(n_clusters=optimal_k, max_iter=max_iters).fit(points)
-    # print(optimal_k)
-    # labels = kmeans.labels_
-    # if optimal_k > 1:
-    #     colors = plt.get_cmap("tab20")(labels / (optimal_k - 1))
-    # else:
-    #     colors = plt.get_cmap("tab20")(labels)
-    # colors = colors[:, :3]
-    # pcd.colors = o3d.utility.Vector3dVector(colors)
-    # # Visualize point cloud with colors assigned by cluster
-    # o3d.visualization.draw_geometries([pcd])
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Display:
+    o3d.visualization.draw_geometries([pcd])
